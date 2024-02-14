a97dc3a4d9f2b76585dbf7232cbf36383d8b962b
Manu Bretelle <chantr4@gmail.com>
2024-02-13 22:32:40 -0800

    docker2rootfs: move to oci-distribution
    oci-distribution crate is actively maintained. Let's use this instead.
    
    Signed-off-by: Manu Bretelle <chantr4@gmail.com>

--- docker2rootfs/src/main.rs
+++ docker2rootfs/src/main.rs
@@ -4,6 +4,11 @@ use std::path::PathBuf;
 use anyhow::{Context, Result};
 use clap::Parser;
 use futures::future::try_join_all;
+use oci_distribution::{
+    manifest::{OciImageManifest, OciManifest},
+    secrets::RegistryAuth,
+    Client, Reference,
+};
 use tracing::{debug, info, trace};
 
 mod render;
@@ -39,23 +44,27 @@ struct Args {
     output: PathBuf,
 }
 
-async fn download_layer(
-    client: &dkregistry::v2::Client,
-    image: &str,
-    layer_digest: &str,
-    cache_dir: Option<&PathBuf>,
-) -> Result<Vec<u8>> {
-    if cache_dir.is_some() {
-        let cache_path = cache_dir.unwrap().join(layer_digest);
+async fn download_layer(client: &Client, args: &Args, layer_digest: &str) -> Result<Vec<u8>> {
+    if args.cache.is_some() {
+        let cache_path = args.cache.as_ref().unwrap().join(layer_digest);
         if cache_path.exists() {
             debug!("Using cached layer {}", layer_digest);
             return fs::read(cache_path)
                 .context(format!("Failed to read cached layer {}", layer_digest));
         }
     }
-    let blob = client.get_blob(image, layer_digest).await?;
-    if cache_dir.is_some() {
-        let cache_path = cache_dir.unwrap().join(layer_digest);
+    let reference = Reference::with_digest(
+        args.registry.clone(),
+        args.image.clone(),
+        layer_digest.to_string(),
+    );
+
+    let mut blob: Vec<u8> = Vec::new();
+    client
+        .pull_blob(&reference, layer_digest, &mut blob)
+        .await?;
+    if args.cache.is_some() {
+        let cache_path = args.cache.as_ref().unwrap().join(layer_digest);
         debug!("Caching layer {}", layer_digest);
         fs::write(cache_path, &blob).context(format!("Failed to cache layer {}", layer_digest))?;
     }
@@ -67,21 +76,47 @@ async fn download_layer(
 /// Otherwise, return the Image manifest originally returned by the API.
 /// Errors if this is an OCI image list and no architecture flag was passed, or if no image manifest matches the architecture.
 async fn get_manifest(
-    client: &dkregistry::v2::Client,
+    client: &mut Client,
+    auth: &RegistryAuth,
     args: &Args,
-) -> Result<dkregistry::v2::manifest::Manifest> {
-    let mut manifest = client.get_manifest(&args.image, &args.reference).await?;
+) -> Result<OciImageManifest> {
+    // Build a URL which will be accepted by oci-distribution.
+    let reference = Reference::with_tag(
+        args.registry.clone(),
+        args.image.clone(),
+        args.reference.clone(),
+    );
+    let (manifest, _) = client
+        .pull_manifest(&reference, auth)
+        .await
+        .expect("Cannot pull manifest");
+
     match manifest {
-        dkregistry::v2::manifest::Manifest::ML(ml) => {
-            debug!("Got an Image List, finding matching platform.");
-            trace!("Image List: {:?}", ml);
+        OciManifest::ImageIndex(ii) => {
+            debug!("Got an OCI Image Index, finding matching platform.");
+            trace!("Image List: {:?}", ii);
             if args.architecture.is_none() {
-                return Err(anyhow::anyhow!("This image is availale for multiple architectures, you need to specify which one you want with --architecture flag."));
+                return Err(anyhow::anyhow!("This image is available for multiple architectures, you need to specify which one you want with --architecture flag."));
             }
-            for item in ml.manifests {
-                if Some(item.architecture()) == args.architecture {
-                    manifest = client.get_manifest(&args.image, &item.digest).await?;
-                    return Ok(manifest);
+            for item in ii.manifests {
+                if item
+                    .platform
+                    .is_some_and(|p| Some(p.architecture) == args.architecture)
+                {
+                    let reference = Reference::with_digest(
+                        args.registry.clone(),
+                        args.image.clone(),
+                        item.digest,
+                    );
+
+                    let (manifest, _) = client
+                        .pull_manifest(&reference, auth)
+                        .await
+                        .expect("Cannot pull manifest");
+                    match manifest {
+                        OciManifest::Image(manifest) => return Ok(manifest),
+                        _ => unreachable!(),
+                    }
                 }
             }
             Err(anyhow::anyhow!(
@@ -89,7 +124,7 @@ async fn get_manifest(
                 args.architecture.as_ref().unwrap()
             ))
         }
-        _ => Ok(manifest),
+        OciManifest::Image(manifest) => Ok(manifest),
     }
 }
 
@@ -117,31 +152,27 @@ async fn main() -> Result<()> {
         anyhow::bail!("Output directory {} is not empty", args.output.display());
     }
 
-    let client = dkregistry::v2::Client::configure()
-        .registry(&args.registry)
-        .insecure_registry(false)
-        .build()?;
-
-    let login_scope = format!("repository:{}:pull", args.image);
-    let dclient = client.authenticate(&[&login_scope]).await?;
-    let manifest = get_manifest(&dclient, &args).await.unwrap_or_else(|_| {
-        panic!(
-            "Did not find a manifest for {}:{}",
-            args.image, args.reference
-        )
+    let auth = RegistryAuth::Anonymous;
+    let mut client = Client::new(oci_distribution::client::ClientConfig {
+        ..Default::default()
     });
 
+    let manifest = get_manifest(&mut client, &auth, &args).await?;
+
     trace!("Manifest: {:?}", manifest);
-    let layers_digests = manifest.layers_digests(None)?;
+
+    let layers_digests: Vec<_> = manifest
+        .layers
+        .iter()
+        .map(|layer| layer.digest.clone())
+        .collect();
 
     trace!("Layers digests: {:?}", layers_digests);
     info!("Downloading {} layer(s)", layers_digests.len());
 
     let blob_futures = layers_digests
         .iter()
-        .map(|layer_digest| {
-            download_layer(&dclient, &args.image, layer_digest, args.cache.as_ref())
-        })
+        .map(|layer_digest| download_layer(&client, &args, layer_digest))
         .collect::<Vec<_>>();
     let blobs = try_join_all(blob_futures).await?;
 
@@ -151,5 +182,6 @@ async fn main() -> Result<()> {
 
     let canonical_path = args.output.canonicalize().unwrap();
     render::unpack(&blobs, &canonical_path)?;
+
     Ok(())
 }
